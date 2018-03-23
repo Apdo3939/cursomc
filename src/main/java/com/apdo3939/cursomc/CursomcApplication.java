b51fb6bb20bbb8b1b6cad47582e7c5fe0f664122
Alexandre <apdo3939@hotmail.com>
2018-03-22 21:16:11 -0300

    Erro

--- src/main/java/com/apdo3939/cursomc/CursomcApplication.java
+++ src/main/java/com/apdo3939/cursomc/CursomcApplication.java
@@ -1,12 +1,36 @@
 package com.apdo3939.cursomc;
 
+import java.lang.reflect.Array;
+import java.util.*;
+import org.assertj.core.util.Arrays;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import com.apdo3939.cursomc.domain.Categoria;
+import com.apdo3939.cursomc.repositories.CategoriaRepository;
+
 
 @SpringBootApplication
-public class CursomcApplication {
+public class CursomcApplication implements CommandLineRunner {
 
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
 	public static void main(String[] args) {
+		
 		SpringApplication.run(CursomcApplication.class, args);
+		
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		 
+		Categoria cata = new Categoria(null, "Informatica");
+		Categoria catb = new Categoria(null, "Escritório");
+		
+		this.categoriaRepository.saveAll(Arrays.asList(cata,catb));
+
+		
 	}
 }
