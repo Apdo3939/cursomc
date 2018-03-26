6cd0d91496f4933a89a2294b9951022d73dca17c
Alexandre <apdo3939@hotmail.com>
2018-03-25 21:18:03 -0300

    Criado Classe Produto e o relacionamento entre a mesma e categoria

--- src/main/java/com/apdo3939/cursomc/CursomcApplication.java
+++ src/main/java/com/apdo3939/cursomc/CursomcApplication.java
@@ -1,14 +1,18 @@
 package com.apdo3939.cursomc;
 
-import java.lang.reflect.Array;
-import java.util.*;
-import org.assertj.core.util.Arrays;
+
+
+import java.util.Arrays;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 import com.apdo3939.cursomc.domain.Categoria;
+import com.apdo3939.cursomc.domain.Produto;
 import com.apdo3939.cursomc.repositories.CategoriaRepository;
+import com.apdo3939.cursomc.repositories.ProdutoRepository;
 
 
 @SpringBootApplication
@@ -16,6 +20,8 @@ public class CursomcApplication implements CommandLineRunner {
 
 	@Autowired
 	private CategoriaRepository categoriaRepository;
+	@Autowired
+	private ProdutoRepository produtoRepository;
 	
 	public static void main(String[] args) {
 		
@@ -26,10 +32,33 @@ public class CursomcApplication implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 		 
-		Categoria cata = new Categoria(null, "Informatica");
-		Categoria catb = new Categoria(null, "Escritório");
+		Categoria cat1 = new Categoria(null, "Informatica");
+		Categoria cat2 = new Categoria(null, "Escritório");
+		Categoria cat3 = new Categoria(null, "Games");
+		Categoria cat4 = new Categoria(null, "Video Games");
+		Categoria cat5 = new Categoria(null, "Acessórios");
 		
-		this.categoriaRepository.saveAll(Arrays.asList(cata,catb));
+		Produto p1 = new Produto(null,"Computador", 2000.00);
+		Produto p2 = new Produto(null,"Impressora",  800.00);
+		Produto p3 = new Produto(null,"Mouse",        80.00);
+		Produto p4 = new Produto(null,"Xbox one X", 4000.00);
+		Produto p5 = new Produto(null,"Halo 5",      100.00);
+		
+		cat1.getProdutos().addAll(Arrays.asList(p1,p2,p3));
+		cat2.getProdutos().addAll(Arrays.asList(p2));
+		cat3.getProdutos().addAll(Arrays.asList(p5));
+		cat4.getProdutos().addAll(Arrays.asList(p4));
+		cat5.getProdutos().addAll(Arrays.asList(p3,p2));
+		
+		p1.getCategorias().addAll(Arrays.asList(cat1));
+		p2.getCategorias().addAll(Arrays.asList(cat1,cat2));
+		p3.getCategorias().addAll(Arrays.asList(cat1,cat2,cat3));
+		p4.getCategorias().addAll(Arrays.asList(cat3,cat4));
+		p5.getCategorias().addAll(Arrays.asList(cat3,cat4));
+		
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1,cat2,cat3,cat4,cat5 ));
+		produtoRepository.saveAll(Arrays.asList(p1,p2,p3,p4,p5));
 
 		
 	}
--- src/main/java/com/apdo3939/cursomc/domain/Categoria.java
+++ src/main/java/com/apdo3939/cursomc/domain/Categoria.java
@@ -1,11 +1,14 @@
 package com.apdo3939.cursomc.domain;
 
 import java.io.Serializable; //Transformer for bytes
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToMany;
 
 @Entity
 public class Categoria implements Serializable{
@@ -16,6 +19,8 @@ public class Categoria implements Serializable{
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Integer id;
 	private String nome;
+	@ManyToMany(mappedBy = "categorias")
+	private List<Produto> produtos = new ArrayList<>();
 	
 	//Constructor
 	public Categoria() {
@@ -46,7 +51,15 @@ public class Categoria implements Serializable{
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
-	
+		
+	public List<Produto> getProdutos() {
+		return produtos;
+	}
+
+	public void setProdutos(List<Produto> produtos) {
+		this.produtos = produtos;
+	}
+
 	//to string
 	@Override
 	public String toString() {
